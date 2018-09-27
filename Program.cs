00479c79e4591fe9e77918e132254ee521f47151
Murilo Dos Santos Gregorio <50981737870@info.local>
2018-09-27 09:12:44 -0300

    mensagem

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace senai.operadores.ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Média de preço dos produtos");
+
+            Console.WriteLine("informe o preço do 1° produto");
+            float prod1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("informe o preço do 2° produto");
+            float prod2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("informe o preço do 3° produto");
+            float prod3 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("informe o preço do 4° produto");
+            float prod4 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("informe o preço do 5° produto");
+            float prod5 = float.Parse(Console.ReadLine());
+
+            float total = ( prod1 + prod2 + prod3 + prod4 + prod5 );
+
+            Console.WriteLine("O total da compra é: " + total);
+            
+            float media = (float)( total/ 5 );
+
+            Console.WriteLine("O Gasto médio é: " + media);
+        }
+    }
+}
